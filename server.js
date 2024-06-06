8a04af4b4915007062a2bf4fd472a993b1aea55c
Anson Wong <cutietik@gmail.com>
2024-06-06 11:47:20 +0800

    Support require header

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ var port = process.env.PORT || 8080;
 // use originWhitelist instead.
 var originBlacklist = parseEnvList(process.env.CORSANYWHERE_BLACKLIST);
 var originWhitelist = parseEnvList(process.env.CORSANYWHERE_WHITELIST);
+var requireHeader = parseEnvList(process.env.CORSANYWHERE_REQUIRE_HEADER);
 function parseEnvList(env) {
   if (!env) {
     return [];
@@ -23,7 +24,7 @@ var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
   originBlacklist: originBlacklist,
   originWhitelist: originWhitelist,
-  requireHeader: ['origin', 'x-requested-with'],
+  requireHeader: requireHeader,
   checkRateLimit: checkRateLimit,
   removeHeaders: [
     'cookie',
